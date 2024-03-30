0ab0b5ecf5ca9dec9dabb472a8517554a035fcb3
Cole Tran <44384202+mathlover1998@users.noreply.github.com>
2024-03-30 10:23:15 +0700

    create add_to_cart function

--- fruit_shop/product_manager/urls.py
+++ fruit_shop/product_manager/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.product_view, name="product_view"),
     path("create/", views.create_product, name="create_product"),
-    path("<int:product_id>",views.product_detail,name="product_detail")
+    path("<int:product_id>",views.product_detail,name="product_detail"),
+    path('cart',views.cart_view,name='cart_view'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    
+    
 ]
--- fruit_shop/product_manager/views.py
+++ fruit_shop/product_manager/views.py
@@ -5,6 +5,7 @@ from fruit_shop.utils import position_required
 from .forms import CreateProductForm
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -45,5 +46,40 @@ def product_detail(request,product_id):
     product = Product.objects.filter(pk=product_id).first()
     return render(request,'shop/product_detail.html',{'product':product})
 
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    product = get_object_or_404(Product, pk=product_id)
+    quantity = 1  # Default quantity to 1 if not specified
+    if product.id in cart:
+        cart[product.id]['quantity'] += quantity
+    else:
+        cart[product.id] = {'product_id': product.id, 'quantity': quantity}
+    request.session['cart'] = cart
+    # Redirect to the shop page or wherever appropriate
+    print(cart)
+    return redirect(reverse('product_view'))
+
+def cart_view(request):
+    cart = request.session.get('cart', {})
+    cart_items = []
+    total_price = 0
+    
+    # Retrieve product details for items in the cart
+    for item_id, item_data in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        quantity = item_data['quantity']
+        item_total_price = product.price * quantity
+        total_price += item_total_price
+        cart_items.append({'product': product, 'quantity': quantity, 'total_price': item_total_price})
+    
+    context = {
+        'cart_items': cart_items,
+        'total_price': total_price
+    }
+    return render(request,'shop/cart.html',context)
+
+
+
 def best_seller(request):
     return render(request,'shop/gallery.html')
+
