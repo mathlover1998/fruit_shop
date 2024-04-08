d7583ea1fe51908d78f1a27cd989b1c9e1378e00
Cole Tran <44384202+mathlover1998@users.noreply.github.com>
2024-04-08 07:54:09 +0700

    connect to aws database

--- fruit_shop/fruit_shop/settings.py
+++ fruit_shop/fruit_shop/settings.py
@@ -107,12 +107,22 @@ WSGI_APPLICATION = 'fruit_shop.wsgi.application'
 # }
 
 DATABASES = {
+    # local postgresql database
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'fruit_shop_db',
+    #     'USER': 'admin',
+    #     'PASSWORD': '1234',
+    #     'HOST':'localhost',
+    #     'PORT':'5432' 
+    # }
+    # live database
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fruit_shop_db',
-        'USER': 'admin',
-        'PASSWORD': '1234',
-        'HOST':'localhost',
+        'NAME': 'cole_fruit_shop_db',
+        'USER': 'coletran',
+        'PASSWORD': 'Bunnie123',
+        'HOST':'database-2.clbdt0uzsyg9.ap-southeast-2.rds.amazonaws.com',
         'PORT':'5432' 
     }
 }
