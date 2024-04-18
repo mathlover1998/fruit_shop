8a42a145e1d605d2544d75ee02e52309f6960325
Cole Tran <44384202+mathlover1998@users.noreply.github.com>
2024-04-18 18:32:26 +0700

    makemigrations blog models

--- fruit_shop/blog/views.py
+++ fruit_shop/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
+from django.http import HttpResponse
 # Create your views here.
+
+def blog_list_view(request):
+    return HttpResponse('Noob')
